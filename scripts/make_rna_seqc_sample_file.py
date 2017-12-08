69c6b7483ce3f32a7b1af9278fac066e8d938054
Masashi Fujita <mssfjt@gmail.com>
2017-12-09 02:11:38 +0900

    added notes

--- scripts/make_rna_seqc_sample_file.py
+++ scripts/make_rna_seqc_sample_file.py
@@ -8,4 +8,4 @@ with open(snakemake.output[0], "w") as f:
     writer.writerow(["Sample ID", "Bam File", "Notes"])
     for bam in bams:
         sample_id=os.path.basename(os.path.dirname(bam))
-        writer.writerow([sample_id, bam, ""])
+        writer.writerow([sample_id, bam, "NA"])
